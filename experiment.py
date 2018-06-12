0324c2cec5158acab5fb13a00399a033f182e4c3
Monolith <biomega.432@gmail.com>
2018-06-12 11:44:54 +0530

    Sacred integration, redesign of trainer script

---
+++ experiment.py
@@ -0,0 +1,39 @@
+from sacred import Experiment
+
+import model
+import dataset
+
+ex = Experiment('language')
+
+
+@ex.config
+def config():
+    model_type = 'seq2seq'
+    params = {'max_sequence_len':None,
+              'units':[1024],
+              'rnn_params':{'recurrant_dropout':0.5}}
+    optimizer = 'nadam'
+    loss = 'categorical_crossentropu'
+
+
+@ex.capture
+def build_model(vocabulary_size, model_type, params, optimizer, loss):
+    builder = getattr(model, f'build_{model_type}')
+    params['vocabulary_size'] = vocabulary_size
+    nmt = builder(**params)
+    nmt.compile(optimizer=optimizer, loss=loss)
+    return nmt
+
+
+@ex.capture
+def train_model(epochs, verbose):
+    train, test, dictionary = dataset.etl()
+    vocabulary = tuple(i.last_word_idx+2 for i in dictionary)
+    language_model = build_model(vocabulary)
+    language_model.fit_generator(train, epochs=epochs, verbose=verbose,
+                                 validation_data=test)
+
+
+@ex.main
+def run():
+    train_model()
--- train.py
+++ train.py
@@ -1,14 +1,5 @@
-import dataset
-import model
-
-
-def train():
-    train, test, dictionary = dataset.etl()
-    vocabulary = tuple(i.last_word_idx+2 for i in dictionary)
-    nmt = model.build_seq2seq(None, vocabulary_size=vocabulary, units=[1024]*2, rnn_params={'recurrent_dropout':0.5})
-    nmt.compile(optimizer='nadam', loss='categorical_crossentropy')
-    nmt.fit_generator(train, epochs=10, verbose=1, validation_data=test)
+from experiment import ex
 
 
 if __name__ == '__main__':
-    train()
+    ex.run(config_updates={'epochs':3})
